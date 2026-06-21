517b87d4a99965e28384e02d3da9be33a87101f8
superdtf-0882 <superdtf@gmail.com>
2026-06-21 12:27:01 -0600

    Update debug-env to test blob list and capture error

--- api/debug-env.js
+++ api/debug-env.js
@@ -1,10 +1,24 @@
 'use strict';
 
-module.exports = function handler(req, res) {
+module.exports = async function handler(req, res) {
   const blobKeys = Object.keys(process.env).filter(k => k.includes('BLOB') || k.includes('VERCEL_OIDC'));
+
+  let blobListResult = null;
+  let blobListError = null;
+  try {
+    const { list } = await import('@vercel/blob');
+    const page = await list({ prefix: 'responses/', limit: 5 });
+    blobListResult = { blobCount: page.blobs.length, hasMore: page.hasMore };
+  } catch (err) {
+    blobListError = err.message;
+  }
+
   res.status(200).json({
     blobKeys,
     hasBlobReadWriteToken: !!process.env.BLOB_READ_WRITE_TOKEN,
     hasVercelOidc: !!process.env.VERCEL_OIDC_TOKEN,
+    hasBlobStoreId: !!process.env.BLOB_STORE_ID,
+    blobListResult,
+    blobListError,
   });
 };
